9c58743b11091d7bac5167f2cc0a9dbedfec28cf
Daniela Pina <you@example.com>
2022-09-17 00:57:37 -0500

    adding ex01

---
+++ ex01.js
@@ -0,0 +1,15 @@
+const printcombinations = (digits) => {
+
+  let perms = [];
+
+  for (let i = 0; i < digits.length; i++) {
+    let digit = digits[i];
+    perms.push(digit);
+
+    //using recursivity 
+    for (let p of printcombinations(digits.slice(0, i).concat(digits.slice(i + 1)))) {
+      perms.push(digit + p);
+    }
+  }
+  return perms;
+};
